7dbdbc28b493040ebb079a6d017115fdc3884e48
1sfaccia1 <chad.m.wittman75@gmail.com>
2020-08-23 19:53:01 -0500

    connected mongoDB

--- server/app.js
+++ server/app.js
@@ -1,7 +1,9 @@
 const express = require('express');
-const app = express()
-const port = process.env.PORT || 8000;
+const app = express();
+const connectMongoDB = require('./utils/connectMongoDB');
 
+const port = process.env.PORT || 8000;
+connectMongoDB();
 app.listen(port, () => {
   console.log(`Server started on ${port}`);
 });
